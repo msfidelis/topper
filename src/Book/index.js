871d3c1836c595b634a577339d85e00811088317
Matheus Fidelis <msfidelis@outlook.com>
2018-05-17 12:57:58 -0300

    book service

--- src/Book/index.js
+++ src/Book/index.js
@@ -2,6 +2,7 @@
 
 const net = require('net');
 const json = require('../Parsers/json');
+
 class Book {
 
     constructor(host = '0.0.0.0', port = 6000) {
@@ -13,20 +14,17 @@ class Book {
 
     async register(jobs) {
    
-        await jobs.forEach(job => {
-            this.actions[job.name] = job.task;
-        });
-
-        console.log(this.actions);
+        await jobs.forEach(job => this.actions[job.name] = job.task);
         
         net.createServer(socket => {
             socket.on('data', async data => {
-                const message   = await data.toString('utf8');
+                const message   = await data.toString('utf8').replace(/[\n\r]*$/, '');
                 const action    = await message.replace(/ .*/,'');
                 const payload   = await json(message.substr(message.indexOf(" ") + 1));
-                console.log(this.actions[action], console.log(action));
+                if (this.actions[action]) this.actions[action](payload, socket);
             });
         }).listen(this.port, this.host);
+
     };
 
 }
