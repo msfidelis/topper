59a67bdcf08d005d89c8257019702e354f2c62d9
Matheus Fidelis <msfidelis@outlook.com>
2018-05-18 10:22:12 -0300

    create a simple multi tasking server

--- src/Server/index.js
+++ src/Server/index.js
@@ -3,14 +3,27 @@
 const net   = require('net');
 const json  = require('../Parsers/json');
 
+/**
+ * 
+ */
 class Server {
 
+    /**
+     * Server Constructor
+     * @param {*} host address 
+     * @param {*} port 
+     */
     constructor(host = '127.0.0.1', port = 6960) {
         this.host = host;
         this.port = port;
+        this.actions = [];
         console.log(`New task registered on tcp://${host}:${port}`);
     };
 
+    /**
+     * Register a simple TCP task on server
+     * @param {*} task 
+     */
     task(task) {
         net.createServer(socket => {
             socket.on('data', async data => {
@@ -19,6 +32,25 @@ class Server {
             });
         }).listen(this.port, this.host);
     };
+
+    /**
+     * Register a multiple tasks on server
+     * @param {*} jobs 
+     */
+    multiTask(jobs) {
+
+        jobs.forEach(job => this.actions[job.name] = job.task);
+
+        net.createServer(socket => {
+            socket.on('data', async data => {
+                const message   = await data.toString('utf8').replace(/[\n\r]*$/, '');
+                const action    = await message.replace(/ .*/,'');
+                const payload   = await json(message.substr(message.indexOf(" ") + 1));
+                if (this.actions[action]) this.actions[action](payload, socket);
+            });
+        }).listen(this.port, this.host);
+
+    }
 }
 
 module.exports = Server;
--- test.js
+++ test.js
@@ -1,31 +1,39 @@
 'use strict';
 
-const { Server } = require('./');
-const server = new Server('0.0.0.0', 4000);
+const { Book } = require('./');
+// const server = new Server('0.0.0.0', 4000);
 
-const task = async (data, server) => {
-    server.write(`hello, my name is ${data.name} and Im ${data.age} old\n`);
-}
+// const task = async (data, server) => {
+//     server.write(`hello, my name is ${data.name} and Im ${data.age} old\n`);
+// }
 
-server.task(task);
+// server.task(task);
 
-// const book = new Book('0.0.0.0', 6000);
+const book = new Book('0.0.0.0', 6000);
 
+const tasks = [
+    {
+        name: 'Ping',
+        task: async (data, server) => await server.write('Pong\n')
+    }, 
+    {
+        // echo 'Sum {"numbers": [1, 2, 3, 4, 5]}' | nc localhost 6000
+        name: 'Sum',
+        task: async (data, server) => {
+            let sum = data.numbers.reduce((prev, curr) => prev + curr);
+            await server.write(`The sum is: ${sum} \n`);
+        }
+    },
+    {   
+        // echo 'Avg {"numbers": [1, 2, 3, 4, 5]}' | localhost 6000
+        name: 'Avg',
+        task: async (data, server) => {
+            let sum = data.numbers.reduce((prev, curr) => prev + curr);
+            let average =  sum / data.numbers.length;
+            server.write(`The sum of numbers is: ${sum} \n`);
+            server.write(`The average is: ${average} \n`);
+        }
+    }
+]
 
-// const tasks = [
-//     {
-//         name: 'Ping',
-//         task: async (data, server) => {
-//             console.log(data);
-//             await server.write('Pong');
-//         }
-//     }, 
-//     {
-//         name: 'Count',
-//         task: async (data, server) => {
-//             await server.write(`Count server`);
-//         }
-//     }
-// ]
-
-// book.register(tasks);
+book.register(tasks);
